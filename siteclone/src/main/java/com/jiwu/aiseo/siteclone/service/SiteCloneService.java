e54d7ed7f7f6080fb32b323043072d91eaa54f5b
leoobai <imleoo@gmail.com>
2025-07-12 17:06:28 +0800

    "Add configuration injection: introduce @Value annotation for property injection in SiteCloneService"

--- siteclone/src/main/java/com/jiwu/aiseo/siteclone/service/SiteCloneService.java
+++ siteclone/src/main/java/com/jiwu/aiseo/siteclone/service/SiteCloneService.java
@@ -9,6 +9,7 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
